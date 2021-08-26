868020dd21c7494c380c6acde385c34882091824
wiedhodho[at]LPSE Office <wiedhodho@gmail.com>
2021-08-26 10:46:56 +0800

    dashboard ok

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,15 +2,23 @@
 
 namespace App\Controllers;
 
-// use App\Models\User;
+use App\Models\Mtransaksi;
+use App\Models\Mcustomer;
 
 class Home extends BaseController {
 	protected $halaman = 'dashboard/';
 
 	public function index() {
-		//$user = new User();
+		$customer = new Mcustomer();
+		$transaksi = new Mtransaksi();
 		$data['home'] = 'oke';
-
+		$data['customer'] = $customer->countAllResults();
+		$data['count_transaksi'] = $transaksi->where('transaksi_status<', 3)->where('transaksi_status>', 0)->countAllResults();
+		$bln_ini = date('Y-m');
+		$data['transaksi_bln_ini'] = $transaksi->like('transaksi_created', $bln_ini, 'after')->where('transaksi_status>', 0)->countAllResults();
+		$data['last_trx'] = $transaksi->select('customer_nama, transaksi_created,(SELECT barang_nama FROM barang WHERE barang_transaksi=transaksi_id LIMIT 1) as barang')
+			->join('customer', 'transaksi_customer=customer_id')->orderBy('transaksi_id', 'DESC')->findAll(5);
+		$data['trx_per_bulan'] = $transaksi->select('count(transaksi_id) as banyak,MONTH(transaksi_created) as bulan')->where('transaksi_status>', 0)->where('YEAR(transaksi_created)', date('Y'))->groupBy('MONTH(transaksi_created)')->findAll();
 		return view($this->halaman . 'index', $data);
 	}
 }
--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -12,7 +12,99 @@
 <script src="<?= base_url() ?>/assets/plugins/flot/jquery.flot.symbol.min.js"></script>
 <script src="<?= base_url() ?>/assets/plugins/flot/jquery.flot.resize.min.js"></script>
 <script src="<?= base_url() ?>/assets/plugins/flot/jquery.flot.tooltip.min.js"></script>
-<script src="<?= base_url() ?>/assets/js/pages/dashboard.js"></script>
+<script>
+    $(document).ready(function() {
+        var options = {
+            series: [{
+                name: 'Total Transaksi',
+                data: [
+                    <?php
+                    for ($i = 1; $i < 13; $i++) {
+                        $ketemu = false;
+                        foreach ($trx_per_bulan as $t) {
+                            if ($i == $t->bulan) {
+                                echo $t->banyak . ',';
+                                $ketemu = true;
+                                break;
+                            }
+                        }
+                        if (!$ketemu) echo '0,';
+                    }
+                    ?>
+                ]
+            }],
+            chart: {
+                height: 350,
+                type: 'bar',
+                toolbar: {
+                    show: false
+                }
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 10,
+                    dataLabels: {
+                        position: 'top', // top, center, bottom
+                    },
+                }
+            },
+            dataLabels: {
+                enabled: true,
+
+                offsetY: -20,
+                style: {
+                    fontSize: '12px',
+                    colors: ["#304758"]
+                }
+            },
+
+            xaxis: {
+                categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+                position: 'bottom',
+                axisBorder: {
+                    show: false
+                },
+                axisTicks: {
+                    show: false
+                },
+                crosshairs: {
+                    fill: {
+                        type: 'gradient',
+                        gradient: {
+                            colorFrom: '#D8E3F0',
+                            colorTo: '#BED1E6',
+                            stops: [0, 100],
+                            opacityFrom: 0.4,
+                            opacityTo: 0.5,
+                        }
+                    }
+                },
+                tooltip: {
+                    enabled: true,
+                }
+            },
+            yaxis: {
+                axisBorder: {
+                    show: false
+                },
+                axisTicks: {
+                    show: false,
+                },
+                labels: {
+                    show: false,
+                    formatter: function(val) {
+                        return val + "%";
+                    }
+                }
+
+            }
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart-visitors"), options);
+        chart.render();
+
+    })
+</script>
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
@@ -35,8 +127,8 @@
                 <div class="card card-transparent stats-card">
                     <div class="card-body">
                         <div class="stats-info">
-                            <h5 class="card-title">123</h5>
-                            <p class="stats-text">Total Users</p>
+                            <h5 class="card-title"><?= $customer ?></h5>
+                            <p class="stats-text">Total Customer</p>
                         </div>
                         <div class="stats-icon change-danger">
                             <i class="material-icons">supervisor_account</i>
@@ -48,11 +140,11 @@
                 <div class="card card-transparent stats-card">
                     <div class="card-body">
                         <div class="stats-info">
-                            <h5 class="card-title">122></h5>
-                            <p class="stats-text">Pengajuan Baru</p>
+                            <h5 class="card-title"><?= $count_transaksi ?></h5>
+                            <p class="stats-text">Belum Selesai</p>
                         </div>
                         <div class="stats-icon change-success">
-                            <i class="material-icons">trending_up</i>
+                            <i class="material-icons">shopping_cart</i>
                         </div>
                     </div>
                 </div>
@@ -61,8 +153,8 @@
                 <div class="card card-transparent stats-card">
                     <div class="card-body">
                         <div class="stats-info">
-                            <h5 class="card-title">131</h5>
-                            <p class="stats-text">Perpanjangan</p>
+                            <h5 class="card-title"><?= $transaksi_bln_ini ?></h5>
+                            <p class="stats-text">Transaksi Bulan Ini</p>
                         </div>
                         <div class="stats-icon change-success">
                             <i class="material-icons">trending_up</i>
@@ -73,19 +165,17 @@
         </div>
         <div class="row">
             <div class="col-lg-4">
-                <div class="card savings-card">
-                    <div class="card-body">
-                        <h5 class="card-title">Masa Berlaku Sertifikat</h5>
-                        <div class="savings-stats text-center">
-                            <h1>5454</h1>
-                        </div>
-                    </div>
-                </div>
                 <div class="card top-products">
                     <div class="card-body">
-                        <h5 class="card-title">Customer Baru<span class="card-title-helper">Jenis</span></h5>
+                        <h5 class="card-title">Transaksi Baru<span class="card-title-helper">Waktu</span></h5>
                         <div class="top-products-list">
-                            trx
+                            <?php foreach ($last_trx as $l) : ?>
+                                <div class="product-item">
+                                    <h5><?= $l->customer_nama ?></h5>
+                                    <span><?= $l->transaksi_created ?></span>
+                                    <span class="product-item-status text-warning" style="font-size: 13px"><?= $l->barang ?></span>
+                                </div>
+                            <?php endforeach ?>
                         </div>
                     </div>
                 </div>
@@ -98,7 +188,7 @@
                             <div class="row">
                                 <div class="col-lg-6">
                                     <div class="visitors-stats-info">
-                                        <p>Grafik Permohonan Tahun <?= date('Y'); ?></p>
+                                        <p>Grafik Transaksi Tahun <?= date('Y'); ?></p>
                                     </div>
                                 </div>
                             </div>
