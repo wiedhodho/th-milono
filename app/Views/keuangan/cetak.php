fdce12bfffdee61ba1a0ab362fb69da4fe808605
wiedhodho[at]LPSE Office <wiedhodho@gmail.com>
2021-09-21 15:21:33 +0800

    width tgl laporan keu

--- app/Views/keuangan/cetak.php
+++ app/Views/keuangan/cetak.php
@@ -106,7 +106,7 @@
                 <th>No</th>
                 <th>Keterangan</th>
                 <th>Transfer</th>
-                <th>Tgl</th>
+                <th width="15%">Tgl</th>
                 <th class="text-right">Debet</th>
                 <th class="text-right">Kredit</th>
             </tr>
